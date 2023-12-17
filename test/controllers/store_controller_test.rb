280f5f7f02ab379b46ca647c6aab851d6741fbfb
nikolatriki <kjofte@yahoo.com>
2023-12-17 10:30:53 +0100

    add store_controller test

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,8 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select "nav a", minimum: 4
+    assert_select "main ul li", 2
+    assert_select "h2", "MyString"
   end
 end
