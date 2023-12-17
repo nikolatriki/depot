084c0637bcb022ebcfd611cdc11842f4a6ab08db
nikolatriki <kjofte@yahoo.com>
2023-12-17 21:11:24 +0100

    generate scaffold cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_03_113152) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_17_200526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
