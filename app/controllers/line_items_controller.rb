b46ed9d44d061b925ba94764c964262139a91766
nikolatriki <kjofte@yahoo.com>
2023-12-18 22:06:18 +0100

    add_product method for summing same lineitems in cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,7 +24,7 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product_id: product.id)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
