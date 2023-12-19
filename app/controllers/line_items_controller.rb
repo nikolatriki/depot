bd4b12deb97c049891c816295f37ba2a58fc2d76
nikolatriki <kjofte@yahoo.com>
2023-12-19 23:09:06 +0100

    add price to lineitems add remove line item from cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,7 +1,7 @@
 class LineItemsController < ApplicationController
   include CurrentCart
   before_action :set_cart, only: [:create]
-  before_action :set_line_item, only: %i[show edit update destroy]
+  before_action :set_line_item, only: %i[ show edit update destroy ]
 
   # GET /line_items or /line_items.json
   def index
@@ -56,20 +56,19 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
+      format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_line_item
+      @line_item = LineItem.find(params[:id])
+    end
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_line_item
-    @line_item = LineItem.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def line_item_params
-    params.require(:line_item).permit(:product_id)
-  end
+    # Only allow a list of trusted parameters through.
+    def line_item_params
+      params.require(:line_item).permit(:product_id)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_18_203445) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_19_194353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_18_203445) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
+    t.decimal "price", precision: 8, scale: 2
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
