e4ccd4c61bc809a78fd4231085acd05feabc6840
nikolatriki <kjofte@yahoo.com>
2023-12-20 22:59:37 +0100

    add css animation for highlight element and apply on line item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.turbo_stream
+        format.turbo_stream { @current_item = @line_item }
         format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @line_item }
         session[:counter] = 0
