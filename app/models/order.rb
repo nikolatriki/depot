54eee7125124eac6459371dde81471859155872c
nikolatriki <kjofte@yahoo.com>
2024-04-04 00:34:42 +0200

    set associations between order, cart, line item, create order, add line items to order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,21 @@
 class Order < ApplicationRecord
+  has_many :line_items, dependent: :destroy
+
+  validates :name, :address, :email, presence: true
+
   enum pay_type: {
-    "Chek" => 0,
+    "Check" => 0,
     "Credit card" => 1,
     "Purchase order" => 2
   }
+
+  validates :pay_type, inclusion: pay_types.keys
+
+  def add_line_items_from_cart(cart)
+    cart.line_items.each do |item|
+      item.cart_id = nil
+      line_items << item
+    end
+  end
+
 end
