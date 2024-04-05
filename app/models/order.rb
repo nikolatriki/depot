885261a3b3d9878f36947e762544d8e1eb7b95e5
nikolatriki <kjofte@yahoo.com>
2024-04-05 13:38:38 +0200

    add validations to the new fields and tied up the disable for checkout

--- app/models/order.rb
+++ app/models/order.rb
@@ -10,6 +10,10 @@ class Order < ApplicationRecord
   }
 
   validates :pay_type, inclusion: pay_types.keys
+  validates :routing_number, :account_number, presence: true, if: -> { pay_type == "Check" }
+  validates :credit_card_number, :expiration_date, presence: true, if: -> { pay_type == "Credit card" }
+  validates :po_number, presence: true, if: -> { pay_type == "Purchase order" }
+
   validate :must_have_line_items
 
   def add_line_items_from_cart(cart)
