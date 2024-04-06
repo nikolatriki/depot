cfe2c485e6a560ad44cd6d2126103a1e9503f4f0
nikolatriki <kjofte@yahoo.com>
2024-04-06 15:09:48 +0200

    update mailer tests

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -2,19 +2,19 @@ require "test_helper"
 
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "Order Confirmation", mail.subject
+    assert_equal ["goran@example.org"], mail.to
+    assert_equal ["ristonekoj@gmail.com"], mail.from
+    assert_match /Thank you for your recent order from The Arsov Winery Online Store./, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Order Shipped", mail.subject
+    assert_equal ["goran@example.org"], mail.to
+    assert_equal ["ristonekoj@gmail.com"], mail.from
+    assert_match "Thank you for shopping with us!", mail.body.encoded
   end
 
 end
