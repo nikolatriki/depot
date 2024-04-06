51c82e9ca11c44f4782117be99d23d6952f09b45
nikolatriki <kjofte@yahoo.com>
2024-04-06 14:47:44 +0200

    create mailer order and config smtp

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -32,6 +32,9 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
 
+        OrderMailer.received(@order).deliver_later
+        OrderMailer.shipped(@order).deliver_later
+
         format.html { redirect_to store_index_url, notice: "Thank you for your order!" }
         format.json { render :show, status: :created, location: @order }
       else
