31d871a73d758377d92c3aec3401d815d730dc26
nikolatriki <kjofte@yahoo.com>
2024-04-08 18:55:39 +0200

    add authorize callback to app and session tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,18 +1,26 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  test "should get login page" do
+    get login_url
     assert_response :success
   end
 
-  test "should get create" do
-    get sessions_create_url
-    assert_response :success
+  test "should login" do
+    user = users(:one)
+    post login_url, params: { name: user.name, password: "secret" }
+    assert_redirected_to admin_index_url
+    assert_equal user.id, session[:user_id]
   end
 
-  test "should get destroy" do
-    get sessions_destroy_url
-    assert_response :success
+  test "should fail login" do
+    user = users(:one)
+    post login_url, params: { name: user.name, password: "wrong" }
+    assert_redirected_to login_url
+  end
+
+  test "should log off user" do
+    delete logout_url
+    assert_redirected_to store_index_url
   end
 end
