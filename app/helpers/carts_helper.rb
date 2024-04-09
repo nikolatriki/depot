78cf9c94350e8a19b58fceede003a03405031cf9
nikolatriki <kjofte@yahoo.com>
2024-04-09 20:49:58 +0200

    tweak disable_checkout_button?

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,5 +1,5 @@
 module CartsHelper
   def disable_checkout_button?
-    (request.referer == new_order_url) || current_page?(new_order_path)
+    (request.referer == new_order_url unless current_page?(store_index_path)) || current_page?(new_order_path)
   end
 end
